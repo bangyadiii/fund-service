e0ff75e740764078160def61a6a77e452e778aa5
triadi_arch <triadilaksamana@gmail.com>
2022-10-07 19:57:54 +0700

    add primary checking before preload

--- campaign/repository.go
+++ campaign/repository.go
@@ -16,9 +16,9 @@ func NewRepository(db *gorm.DB) *repository{
 	return &repository{db}
 }
 
-func (r *repository) FindALl() ([] Campaign, error){
+func (r *repository) FindAll() ([] Campaign, error){
 	var campaigns []Campaign
-	err := r.db.Find(&campaigns).Error
+	err := r.db.Preload("CampaignImages", "campaign_primary.is_primary = 1").Find(&campaigns).Error
 	if err != nil {
 		return campaigns, err
 	}
