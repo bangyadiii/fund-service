907e178257837072f7fe468307b19bc4c4bd3e07
triadi <triadilaksamana@gmail.com>
2022-11-10 12:57:02 +0700

    init ulangg

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backend-crowdfunding/campaign"
 	"backend-crowdfunding/routes"
 	"backend-crowdfunding/user"
 	"fmt"
@@ -15,9 +16,9 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-	    log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
-	
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_NAME"))
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -26,10 +27,11 @@ func main() {
 		log.Fatal("error : ", err.Error())
 	}
 	db.AutoMigrate(&user.User{})
+	db.AutoMigrate(&campaign.Campaign{})
+	db.AutoMigrate(&campaign.CampaignImage{})
 
 	router := routes.GetRouter(db)
 
-	appAddress := fmt.Sprintf("%s:%s", os.Getenv("APP_ADDRESS"), os.Getenv("PORT")) 
+	appAddress := fmt.Sprintf("%s:%s", os.Getenv("APP_ADDRESS"), os.Getenv("PORT"))
 	router.GinRouter.Run(appAddress)
 }
-
