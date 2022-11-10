47002369c1a1cce423f54f5f0231d5fcc85ab639
triadi <triadilaksamana@gmail.com>
2022-11-10 21:49:03 +0700

    add validation

--- campaign/input.go
+++ campaign/input.go
@@ -3,8 +3,8 @@ package campaign
 type CreateCampaignInput struct {
 	UserID           uint32 `json:"user_id" binding:"required,numeric"`
 	Name             string `json:"name" binding:"required,alphanum"`
-	ShortDescription string `json:"short_description"`
-	Description      string `json:"description" binding:"required"`
+	ShortDescription string `json:"short_description" binding:"alphanum"`
+	Description      string `json:"description" binding:"required,alphanum"`
 	Perks            string `json:"perks"`
 	BackerCount      uint   `json:"backer_count" binding:"required,numeric"`
 	GoalAmount       uint   `json:"goal_amount" binding:"required,numeric"`
